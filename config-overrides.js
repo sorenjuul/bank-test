a387680160fc1280b9306721ce0f4a5ad4678af6
Søren Juul <sorenjuul@gmail.com>
2020-01-11 00:15:54 +0100

    Upgraded CRA and rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,10 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const {
+  override,
+  fixBabelImports
+} = require('customize-cra');
 
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
-  return config;
-};
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd', libraryDirectory: 'es', style: 'css'
+  })
+);
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -4,9 +4,7 @@ export function getImageUrls(data) {
   const photo = get(data, 'photos.photo', []);
   const imageUrls = photo.map(photoItem => {
     return {
-      url: `https://farm${photoItem.farm}.staticflickr.com/${photoItem.server}/${photoItem.id}_${
-        photoItem.secret
-      }_q.jpg`,
+      url: `https://farm${photoItem.farm}.staticflickr.com/${photoItem.server}/${photoItem.id}_${photoItem.secret}_q.jpg`,
       id: photoItem.id,
       title: photoItem.title,
     };
